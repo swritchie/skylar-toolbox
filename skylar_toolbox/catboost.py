39139c69cc3efe09b4b0cdee994b303d4e610726
Skylar Ritchie <skylarwritchie@gmail.com>
2023-05-16 14:42:09 -0700

    Update defaults

--- skylar_toolbox/catboost.py
+++ skylar_toolbox/catboost.py
@@ -2521,8 +2521,7 @@ def get_parameters(
         'monotone_constraints': {},
         'random_seed': 0,
         'task_type': 'CPU', 
-        'use_best_model': True, 
-        'verbose': 100}
+        'use_best_model': True}
     implemented_model_types_lt = ['classification', 'regression']
     if model_type_sr == 'classification':
         model_defaults_dt = {
@@ -2539,5 +2538,6 @@ def get_parameters(
     else:
         raise NotImplementedError(f'Implemented values of model_type_sr are {implemented_model_types_lt}')
     general_defaults_dt.update(cat_boost_dt)
+    general_defaults_dt['verbose'] = general_defaults_dt['iterations'] // 10
     return general_defaults_dt
     
