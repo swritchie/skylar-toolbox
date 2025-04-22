da59c5663abd2d999e6616f54f9c0846fec21c80
Skylar Ritchie <sritchie@gopfs.com>
2025-04-22 13:25:42 -0600

    Add write_readme()

--- skylar_toolbox/utils.py
+++ skylar_toolbox/utils.py
@@ -25,3 +25,15 @@ def time_callable(callable_):
         print(f'{callable_.__qualname__} - {str(datetime.datetime.now() - now_dt)}')
         return result
     return wrap_callable
+
+# =============================================================================
+# write_readme
+# =============================================================================
+
+def write_readme(outputs_directory_ph):
+    ph = outputs_directory_ph.joinpath('README.md')
+    paths_lt = sorted(outputs_directory_ph.glob(pattern='*.png'))
+    names_lt = list(map(lambda x: x.name, paths_lt))
+    stems_lt = list(map(lambda x: x.stem, paths_lt))
+    data_sr = '\n'.join(map(lambda x, y: '# %s\n![](%s)' % (x, y), stems_lt, names_lt))
+    ph.write_text(data=data_sr)
