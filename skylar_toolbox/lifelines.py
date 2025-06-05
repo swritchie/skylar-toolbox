168e7fd9e5d0fdb7d25d76735e7ad7b5911f53d6
Skylar Ritchie <sritchie@gopfs.com>
2025-06-05 12:02:53 -0600

    Add plot_feature() function

--- skylar_toolbox/lifelines.py
+++ skylar_toolbox/lifelines.py
@@ -4,6 +4,7 @@
 
 import lifelines
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn import base as snbe
 
 # =============================================================================
@@ -27,3 +28,27 @@ class DurationCalculator(snbe.BaseEstimator, snbe.TransformerMixin):
     def _get_duration(start_sr, end_sr): return f'time_from_{start_sr}_to_{end_sr}'
     @staticmethod
     def _get_event_flag(end_sr): return f'{end_sr}_flag'
+
+# =============================================================================
+# plot_feature
+# =============================================================================
+
+def plot_feature(
+    feature_ss, duration_ss, event_flag_ss,
+    Fitter=lifelines.KaplanMeierFitter, init_dt={}, fit_dt={}, plot_dt={}, add_at_risk_counts_dt={}):
+    fitters_lt = []
+    ax = plt.subplot(1, 1, 1)
+    for value in feature_ss.drop_duplicates().sort_values():
+        # Filter
+        filtered_feature_ss = feature_ss.pipe(func=lambda x: x[x.eq(other=value)])
+        fn = lambda x: x.loc[filtered_feature_ss.index]
+        filtered_duration_ss, filtered_event_flag_ss = map(fn, [duration_ss, event_flag_ss])
+        # Initialize
+        fitter = Fitter(**init_dt)
+        # Fit
+        fitter.fit(durations=filtered_duration_ss, event_observed=filtered_event_flag_ss, label=value, **fit_dt)
+        fitters_lt.append(fitter)
+        # Plot
+        fitter.plot(ax=ax, **plot_dt)
+    lifelines.plotting.add_at_risk_counts(*fitters_lt, ax=ax, **add_at_risk_counts_dt)
+    return fitters_lt, ax
