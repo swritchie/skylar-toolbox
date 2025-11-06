18f8719db9ec7b5291bf80ea24ccd0e364299785
Skylar Ritchie <sritchie@gopfs.com>
2025-11-06 11:54:31 -0700

    Remove scikit-plot and yellowbrick (too buggy)

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setuptools.setup(
     author='Skylar Ritchie',
     packages=['skylar_toolbox'],
     zip_safe=False,
-    install_requires=['feature_engine', 'scikit-learn', 'scikit-plot', 'seaborn', 'statsmodels', 'toolz', 'tqdm', 'yellowbrick'],
+    install_requires=['feature_engine', 'scikit-learn', 'seaborn', 'statsmodels', 'toolz', 'tqdm'],
     extras_require={
         'catboost': ['catboost'],
+        'lifelines': ['lifelines'],
         'networkx': ['networkx'],
         'optuna': ['optuna'],
-        'lifelines': ['lifelines'],
         'scikit-survival': ['scikit-survival'],
         'complete': ['catboost', 'lifelines', 'networkx', 'optuna', 'scikit-survival']})
