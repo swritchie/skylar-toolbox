45778b5f54bd51a362ca38df2ff3ac32bb79dcce
Skylar Ritchie <sritchie@gopfs.com>
2025-11-25 17:28:08 -0700

    Add CountVectorizer

--- skylar_toolbox/feature_extraction.py
+++ skylar_toolbox/feature_extraction.py
@@ -6,6 +6,24 @@ import pandas as pd
 from sklearn.feature_extraction import text as snfett
 
 # =============================================================================
+# CountVectorizer
+# =============================================================================
+
+class CountVectorizer(snfett.CountVectorizer):
+    def fit(self, X, y=None):
+        super().fit(raw_documents=X)
+        return self
+    def transform(self, X): return pd.DataFrame(
+        data=super().transform(raw_documents=X).toarray(),
+        columns=self.get_feature_names_out(),
+        index=X.index)
+    def fit_transform(self, X, y=None): return pd.DataFrame(
+        data=super().fit_transform(raw_documents=X).toarray(),
+        columns=self.get_feature_names_out(),
+        index=X.index)
+    def set_output(self, *, transform=None): pass
+
+# =============================================================================
 # TfidfVectorizer
 # =============================================================================
 
