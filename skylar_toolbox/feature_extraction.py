2bc75f08a22f693be904a7fabe11e640e51f47c0
Skylar Ritchie <sritchie@gopfs.com>
2025-11-25 20:38:08 -0700

    Add remove_*() functions for preprocessing

--- skylar_toolbox/feature_extraction.py
+++ skylar_toolbox/feature_extraction.py
@@ -3,6 +3,8 @@
 # =============================================================================
 
 import pandas as pd
+import re
+import string
 from sklearn.feature_extraction import text as snfett
 
 # =============================================================================
@@ -39,3 +41,29 @@ class TfidfVectorizer(snfett.TfidfVectorizer):
         self.fit(X=X, y=y)
         return self.transform(X=X)
     def set_output(self, *, transform=None): pass
+
+# =============================================================================
+# remove_digits
+# =============================================================================
+
+def remove_digits(x, **kwargs): return re.sub(pattern=r'\d', repl='', string=x, **kwargs)
+
+# =============================================================================
+# remove_multiple_spaces
+# =============================================================================
+
+def remove_multiple_spaces(x, **kwargs): return re.sub(pattern=r'\s{2,}', repl=' ', string=x, **kwargs)
+
+# =============================================================================
+# remove_punctuation
+# =============================================================================
+
+def remove_punctuation(x, punctuation_sr=string.punctuation, **kwargs):
+    return re.sub(pattern=fr'[{string.punctuation}]', repl='', string=x, **kwargs)
+
+# =============================================================================
+# remove_stop_words
+# =============================================================================
+
+def remove_stop_words(x, stop_words_st=snfett.ENGLISH_STOP_WORDS): 
+    return ' '.join(y for y in x.split() if y not in stop_words_st)
